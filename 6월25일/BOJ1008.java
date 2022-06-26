e2c8ef431c36a7fdb6c3cc6a14500aff9da49488
윤여빈 <tommy7899@naver.com>
2022-06-26 12:35:47 +0900

    change folder name

---
+++ 6월25일/BOJ1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        sc.close();
+        System.out.print(a/b);
+    }
+}
