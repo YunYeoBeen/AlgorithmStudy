f7fa601af4766751b05dc0de8b34710203cd0638
윤여빈 <tommy7899@naver.com>
2022-07-06 23:22:34 +0900

    2022-07-06 Algorithm

---
+++ 2022-07/0706/BOJ2579.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ5525 {
+    public static void main(String[] args) throws Exception{
+        String[] str = new String[]{"I","O"};
+        String res = "";
+        System.out.println(res);
+    }
+}
